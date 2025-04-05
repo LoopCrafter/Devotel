8036a96ad3b0f57f64bdf988fcaf55e8ff06feb8
Hamed <ostovari.eng@gmail.com>
2025-04-05 19:11:30 +0200

    feat(store): configure redux toolkit store with redux-persist for theme

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import themeReducer from './features/themeToggle/themeSlice'
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = {
+  theme: persistReducer(persistConfig, themeReducer)
+};
 export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
-})
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
